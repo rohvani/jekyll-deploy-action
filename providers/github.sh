cf11b4e94383e7884f09de8d40e853f26def712a
rohvani <3782201+rohvani@users.noreply.github.com>
2023-09-13 13:27:35 -0700

    keep diff log
    test change
    
    update again
    
    switch from mv to cp
    
    add git config
    
    git reset and switch
    
    git checkout
    
    cleanup
    
    add git clean

--- providers/github.sh
+++ providers/github.sh
@@ -26,14 +26,21 @@ fi
 echo "Deploying to ${REPOSITORY} on branch ${BRANCH}"
 echo "Deploying to ${REMOTE_REPO}"
 
-git config --global init.defaultBranch main && \
-  git init && \
+JEKYLL_BUILD_OUTPUT_PATH=${PWD}
+cd $GITHUB_WORKSPACE && \
+  mv $JEKYLL_BUILD_OUTPUT_PATH jek_build_output && \
+  cd $JEKYLL_SRC && \
   git config user.name "${ACTOR}" && \
   git config user.email "${ACTOR}@users.noreply.github.com" && \
-  git add . && \
-  git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
-  git push --force $REMOTE_REPO main:$BRANCH && \
-  (fuser -k .git || rm -rf .git) && \
-  cd ..
+  git reset --hard && git clean  -d  -f . && \
+  git fetch $REMOTE_REPO $BRANCH && \
+  git checkout $BRANCH && \
+  git reset --hard && git clean  -d  -f . && \
+  cp -Rf $GITHUB_WORKSPACE/jek_build_output/* ./ && \
+  # remove artifacts
+  rm -rf jekyll_site/.bundle/ jekyll_site/.jekyll-cache/ jekyll_site/Gemfile.lock jekyll_site/package.json
+  # push it all to the repo
+  git add -A && git commit -m "update site deployment" && \
+  git push --force $REMOTE_REPO $BRANCH:$BRANCH
 
 PROVIDER_EXIT_CODE=$?
